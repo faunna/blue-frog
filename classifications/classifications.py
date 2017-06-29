dd10eba14e8b2590608b236212c1188474c61bd2
fauna <guillermina.bea@icloud.com>
2017-06-29 21:34:39 +0200

    Update classifications.py

--- classifications/classifications.py
+++ classifications/classifications.py
@@ -1,3 +1,22 @@
+*\
+    Copyright (C) 2017. faunna
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    This function is strongly based on http://scikit-learn.org/stable/auto_examples/svm/plot_iris.html
+*/
+
+
 def plot_decision_regions(clf, X, y, fig=None, title='', xlabel='', ylabel='', figsize=(8,6)):
   
  from matplotlib.colors import ListedColormap
